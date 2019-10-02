98e04891fe8a9b017eef902017557b5d9d547a00
Andrea Maugars <andrea.maugars@gmail.com>
2019-10-02 23:43:36 +0200

    Add solution

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,5 +1,14 @@
-const fizzBuzz = () => {
-  // Start here
+const fizzBuzz = number => {
+    if (number % 3 === 0 && number % 5 === 0) {
+        return "fizzbuzz";
+    }
+    if (number % 3 === 0) {
+        return "fizz";
+    }
+    if (number % 5 === 0) {
+        return "buzz";
+    }
+    return number;
 };
 
 module.exports = fizzBuzz;
