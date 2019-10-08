c789aab12d8d9c8fd70b29cd682c04605cae0444
Andrea Maugars <andrea.maugars@gmail.com>
2019-10-08 17:17:24 +0200

    Update tests to use jest

--- fizzBuzz.test.js
+++ fizzBuzz.test.js
@@ -2,43 +2,43 @@ const assert = require("assert");
 const fizzBuzz = require("./fizzBuzz.js");
 
 describe("fizzBuzz", () => {
-  it("Should be loaded", () => {
-    assert.equal(true, true);
-  });
+    it("Should be loaded", () => {
+        expect(true).toBe(true);
+    });
 
-  it("Should return fizz", () => {
-    assert.equal(fizzBuzz(3), "fizz");
-  });
+    it("Should return fizz", () => {
+        expect(fizzBuzz(3)).toBe("fizz");
+    });
 
-  it("Should return fizz", () => {
-    assert.equal(fizzBuzz(6), "fizz");
-    assert.equal(fizzBuzz(18), "fizz");
-  });
+    it("Should return fizz", () => {
+        expect(fizzBuzz(6)).toBe("fizz");
+        expect(fizzBuzz(18)).toBe("fizz");
+    });
 
-  it("Should return buzz", () => {
-    assert.equal(fizzBuzz(5), "buzz");
-  });
+    it("Should return buzz", () => {
+        expect(fizzBuzz(5)).toBe("buzz");
+    });
 
-  it("Should return buzz", () => {
-    assert.equal(fizzBuzz(10), "buzz");
-    assert.equal(fizzBuzz(25), "buzz");
-  });
+    it("Should return buzz", () => {
+        expect(fizzBuzz(10)).toBe("buzz");
+        expect(fizzBuzz(25)).toBe("buzz");
+    });
 
-  it("Should return fizzbuzz", () => {
-    assert.equal(fizzBuzz(15), "fizzbuzz");
-  });
+    it("Should return fizzbuzz", () => {
+        expect(fizzBuzz(15)).toBe("fizzbuzz");
+    });
 
-  it("Should return fizzbuzz", () => {
-    assert.equal(fizzBuzz(30), "fizzbuzz");
-    assert.equal(fizzBuzz(150), "fizzbuzz");
-  });
+    it("Should return fizzbuzz", () => {
+        expect(fizzBuzz(30)).toBe("fizzbuzz");
+        expect(fizzBuzz(150)).toBe("fizzbuzz");
+    });
 
-  it("Should return 2", () => {
-    assert.equal(fizzBuzz(2), "2");
-  });
+    it("Should return 2", () => {
+        expect(fizzBuzz(2)).toBe("2");
+    });
 
-  it("Should return 8 and 52", () => {
-    assert.equal(fizzBuzz(8), "8");
-    assert.equal(fizzBuzz(52), "52");
-  });
+    it("Should return 8 and 52", () => {
+        expect(fizzBuzz(8)).toBe("8");
+        expect(fizzBuzz(52)).toBe("52");
+    });
 });
